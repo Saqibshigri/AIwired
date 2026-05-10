ec5297b43252ffe54f382d72536bb1cc77890f33
saqibshigri <saqibshigri5465@gmail.com>
2026-05-10 23:35:13 +0500

    ttteed

--- src/App.tsx
+++ src/App.tsx
@@ -13,11 +13,11 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename='/AIwired'>
         <Routes>
-          <Route path="/" element={<Index />} />
+          <Route path='/' element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-          <Route path="*" element={<NotFound />} />
+          <Route path='*' element={<NotFound />} />
         </Routes>
       </BrowserRouter>
     </TooltipProvider>
